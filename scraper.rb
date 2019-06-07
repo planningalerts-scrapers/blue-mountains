02946d7b84c21194d12ac8ed228717acd05bdfd4
Matthew Landauer <mlandauer@gmail.com>
2019-06-07 15:41:37 +1000

    Get rid of MORPH_PERIOD support

--- scraper.rb
+++ scraper.rb
@@ -2,19 +2,6 @@ require "icon_scraper"
 
 starting_url = "https://www2.bmcc.nsw.gov.au/DATracking/Pages/XC.Track/SearchApplication.aspx"
 
-case ENV['MORPH_PERIOD']
-  when 'lastmonth'
-  	period = "lastmonth"
-  when 'thismonth'
-  	period = "thismonth"
-  when
-    period = "thisweek"
-  else
-    period = "last14days"
-
-end
-puts "Getting data in `" + period + "`, changable via MORPH_PERIOD environment"
-
 agent = Mechanize.new
 
 doc = agent.get(starting_url)
@@ -23,4 +10,4 @@ button = form.button_with(value: "I Agree")
 raise "Can't find agree button" if button.nil?
 doc = form.submit(button)
 
-IconScraper.rest_xml(starting_url, "d=" + period + "&k=LodgementDate&o=xml", false, agent)
+IconScraper.rest_xml(starting_url, "d=last14days&k=LodgementDate&o=xml", false, agent)
