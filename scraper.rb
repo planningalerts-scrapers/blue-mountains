a11551909e7da6d9a91d2bce2ce677a67abb71c3
Matthew Landauer <mlandauer@gmail.com>
2019-06-07 15:45:49 +1000

    Upgrade to latest version of the gem

--- scraper.rb
+++ scraper.rb
@@ -1,13 +1,3 @@
 require "icon_scraper"
 
-starting_url = "https://www2.bmcc.nsw.gov.au/DATracking/Pages/XC.Track/SearchApplication.aspx"
-
-agent = Mechanize.new
-
-doc = agent.get(starting_url)
-form = doc.forms.first
-button = form.button_with(value: "I Agree")
-raise "Can't find agree button" if button.nil?
-doc = form.submit(button)
-
-IconScraper.rest_xml(starting_url, "d=last14days&k=LodgementDate&o=xml", false, agent)
+IconScraper.scrape_and_save(:blue_mountains)
